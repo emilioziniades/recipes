e90bf00950891f4c1e86a56eed7b8f6c14e4a6fb
Emilio Ziniades <emilioziniades@protonmail.com>
2025-10-14 23:33:31 +0200

    package cooklint with nix

--- flake.nix
+++ flake.nix
@@ -18,44 +18,60 @@
           "aarch64-linux"
           "x86_64-darwin"
           "aarch64-darwin"
-        ] (system: fn nixpkgs.legacyPackages.${system});
+        ] (system: fn system nixpkgs.legacyPackages.${system});
       tag = self.rev or "dirty";
       port = "8080";
     in
     {
-      packages = forEachSystem (pkgs: {
-        container = pkgs.dockerTools.buildLayeredImage {
-          name = "cook-server";
-          tag = tag;
-          contents = [
-            pkgs.cook-cli
-            ./recipes
-          ];
-          config = {
-            Entrypoint = [ "cook" ];
-            Cmd = [
-              "server"
-              "--host"
-              "--port"
-              port
+      packages = forEachSystem (
+        system: pkgs: {
+          container = pkgs.dockerTools.buildLayeredImage {
+            name = "cook-server";
+            tag = tag;
+            contents = [
+              pkgs.cook-cli
+              ./recipes
             ];
-            WorkingDir = ./recipes;
-            ExposedPorts = {
-              port = { };
+            config = {
+              Entrypoint = [ "cook" ];
+              Cmd = [
+                "server"
+                "--host"
+                "--port"
+                port
+              ];
+              WorkingDir = ./recipes;
+              ExposedPorts = {
+                port = { };
+              };
             };
           };
-        };
-      });
 
-      devShells = forEachSystem (pkgs: {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            cook-cli
-            skopeo
-            flyctl
-            just
-          ];
-        };
-      });
+          cooklint = pkgs.rustPlatform.buildRustPackage {
+            name = "cooklint";
+            src = ./cooklint;
+            cargoHash = "sha256-UFMb4i+F87wbpAxe2MSTm53k+UfWSz1sMA4oSbF1cko=";
+          };
+        }
+      );
+
+      devShells = forEachSystem (
+        system: pkgs: {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cook-cli
+              skopeo
+              flyctl
+              just
+            ];
+          };
+          lint = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              just
+              self.packages.${system}.cooklint
+            ];
+          };
+        }
+      );
     };
 }
